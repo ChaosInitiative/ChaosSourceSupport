383976e7aabf13dfb4d09999c9b5db9af8fa4810
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-09-05 15:42:14 +0200

    remove import

--- src/main/java/org/intellij/sdk/language/angelscript/AngelScriptLexerAdapter.java
+++ src/main/java/org/intellij/sdk/language/angelscript/AngelScriptLexerAdapter.java
@@ -1,7 +1,6 @@
 package org.intellij.sdk.language.angelscript;
 
 import com.intellij.lexer.FlexAdapter;
-import org.intellij.sdk.language.angelscript._AngelScriptLexer;
 
 public class AngelScriptLexerAdapter extends FlexAdapter {
 	public AngelScriptLexerAdapter() {
