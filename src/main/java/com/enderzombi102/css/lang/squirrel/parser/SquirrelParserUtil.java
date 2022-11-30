943b561d65d11549f602eaaa179cd49c78a9c72d
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-11-30 17:48:09 +0100

    squirrel init

---
+++ src/main/java/com/enderzombi102/css/lang/squirrel/parser/SquirrelParserUtil.java
@@ -0,0 +1,6 @@
+package com.enderzombi102.css.lang.squirrel.parser;
+
+import com.intellij.lang.parser.GeneratedParserUtilBase;
+
+public class SquirrelParserUtil extends GeneratedParserUtilBase {
+}
--- src/main/kotlin/com/enderzombi102/css/ChaosSourceSupportIcons.kt
+++ src/main/kotlin/com/enderzombi102/css/ChaosSourceSupportIcons.kt
@@ -3,6 +3,8 @@ package com.enderzombi102.css
 import com.intellij.openapi.util.IconLoader
 
 object ChaosSourceSupportIcons {
-	@JvmField
-	val AS_FILE = IconLoader.getIcon( "/icons/as.png", ChaosSourceSupportIcons::class.java )
+	@JvmField val AS_FILE = icon( "/icons/as.png" )
+	@JvmField val SQUIRREL_FILE = icon( "/icons/squirrel.png" )
+
+	fun icon( path: String ) = IconLoader.getIcon( path, ChaosSourceSupportIcons::class.java )
 }
