d308849e1d12b7a420bd37ae4f914c30b7d3fee2
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-12-19 12:24:54 +0100

    updating and removing stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,22 @@
 plugins {
-    id("org.jetbrains.intellij") version "1.10.0"
-    kotlin("jvm") version "1.6.21"
-    id("idea")
-    id("java")
+    id("org.jetbrains.intellij") version "1.11.0"
+    kotlin("jvm") version "1.7.20"
+    idea
+    java
 }
 
-idea {
-    module {
-        generatedSourceDirs.add( file("src/main/gen") )
-    }
-}
+sourceSets.main.get().java.srcDir( "src/main/gen" )
+idea.module.generatedSourceDirs.add( file("src/main/gen") )
 
-group = "com.enderzombi102"
-version = "0.1.0"
-
-repositories {
-    mavenCentral()
-}
+repositories.mavenCentral()
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
-    testImplementation( "org.hamcrest:hamcrest:2.2" )
-    testImplementation("org.assertj:assertj-core:3.23.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+	testImplementation( kotlin("script-runtime") )
+	testImplementation( "org.junit.jupiter:junit-jupiter" )
+	testImplementation( platform( "org.junit:junit-bom:5.9.1" ) )
+	// -- from old code --
+	testImplementation( "org.hamcrest:hamcrest:2.2" )
+	testImplementation("org.assertj:assertj-core:3.23.1")
 }
 
 intellij {
@@ -34,41 +27,28 @@ intellij {
 }
 
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
+	compileJava {
         sourceCompatibility = "17"
         targetCompatibility = "17"
     }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+
+	compileKotlin {
         kotlinOptions.jvmTarget = "17"
     }
 
     patchPluginXml {
         sinceBuild.set("222")
         untilBuild.set("240.*")
-        changeNotes.set(
-            """
-            Add change notes here.<br>
-            <em>most HTML tags may be used</em>
-            """
-        )
+        changeNotes.set( projectDir.resolve("changelog").readText() )
     }
 
     signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+        certificateChain.set( System.getenv("CERTIFICATE_CHAIN") )
+        privateKey.set( System.getenv("PRIVATE_KEY") )
+        password.set( System.getenv("PRIVATE_KEY_PASSWORD") )
     }
 
     publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
-}
-
-sourceSets {
-    main {
-        java {
-            srcDir( "src/main/gen" )
-        }
+        token.set( System.getenv("PUBLISH_TOKEN") )
     }
 }
--- src/main/kotlin/com/enderzombi102/css/ChaosSourceSupportIcons.kt
+++ src/main/kotlin/com/enderzombi102/css/ChaosSourceSupportIcons.kt
@@ -3,8 +3,8 @@ package com.enderzombi102.css
 import com.intellij.openapi.util.IconLoader
 
 object ChaosSourceSupportIcons {
-	@JvmField val AS_FILE = icon( "/icons/as.png" )
-	@JvmField val SQUIRREL_FILE = icon( "/icons/squirrel.png" )
+	val AS_FILE = icon( "/icons/as.png" )
+	val SQUIRREL_FILE = icon( "/icons/squirrel.png" )
 
 	fun icon( path: String ) = IconLoader.getIcon( path, ChaosSourceSupportIcons::class.java )
 }
