a2aaf5b17bb134126c955f73da7b8dfb5f77c655
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-01-21 00:58:25 +0100

    xml stuff, trying to understand

---
+++ src/main/kotlin/com/enderzombi102/css/panorama/xml/PanoramaReferencesContributor.kt
@@ -0,0 +1,10 @@
+package com.enderzombi102.css.panorama.xml
+
+import com.intellij.psi.PsiReferenceContributor
+import com.intellij.psi.PsiReferenceRegistrar
+
+class PanoramaReferencesContributor : PsiReferenceContributor() {
+	override fun registerReferenceProviders( registrar: PsiReferenceRegistrar ) {
+
+	}
+}
